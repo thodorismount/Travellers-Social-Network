3e90a0184fcc06d64debd8bd47115c5445aa822a
ip <papavasioa2@gmail.com>
2020-10-15 12:18:46 +0300

    added files

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import Link from 'react-router-dom/Link';
+//MUI
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+
+export class NavBar extends Component {
+    render() {
+        return (
+           <AppBar>
+               <Toolbar className="nav-container">
+                    <Button color="inherit" component={Link} to="/login" >Login</Button>
+                    <Button color="inherit" component={Link} to="/" >Home</Button>
+                    <Button color="inherit" component={Link} to="/signup" >Sing Up</Button>
+               </Toolbar>
+           </AppBar>
+        )
+    }
+}
+
+export default NavBar
