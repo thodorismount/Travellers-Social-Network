a86878bce264d9fe7502c627a491e7c39b7d0d11
katerinap99 <62098281+katerinap99@users.noreply.github.com>
2020-10-19 02:11:36 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,8 @@ class App extends Component{
         <div className="container">
         
           <Switch>
-              <Route exact path="/" component={home}/>
+              <Route exact path="/" component={login}/>
+              <Route exact path="/home" component={home}/>
               <Route exact path="/login" component={login}/>
               <Route exact path="/signup" component={signup}/>
           </Switch>
