57e29f3ce69817cde47ad4f5eef86ba2a0f81bf0
katerinap99 <62098281+katerinap99@users.noreply.github.com>
2020-10-19 02:00:24 +0300

    Update NavBar.js

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react'
-import {Link} from 'react-router-dom';
-//MUI
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
+import Link from 'react-router-dom/Link';
+import Tooltip from '@material-ui/core/Tooltip';
+import IconButton from '@material-ui/core/IconButton';
+import KeyboardReturn from '@material-ui/icons/KeyboardReturn';
+import Avatar from '@material-ui/core/Avatar';
+import './Navbar.css';
 
 export class NavBar extends Component {
+    
+    state = {clicked: false}
     render() {
         return (
-           <AppBar>
-               <Toolbar className="nav-container">
-                    <Button color="inherit" component={Link} to="/login" >Login</Button>
-                    <Button color="inherit" component={Link} to="/" >Home</Button>
-                    <Button color="inherit" component={Link} to="/signup" >Sing Up</Button>
-               </Toolbar>
-           </AppBar>
+           <nav className="NavbarItems" id="navigationbar">
+               <Tooltip  title="Go to feed page" >
+                 <IconButton >    
+                    <img src="earth2.png" alt="Avatar" className="navbar-logo"></img>
+                 </IconButton>
+                </Tooltip>
+                <Avatar className="navbar-user" src="girl_female_woman_avatar-512.png"></Avatar>
+                <Tooltip  title="View profile">
+                 <IconButton >Name</IconButton>
+                </Tooltip>
+               <Tooltip className="navbar-logout" title="Logout" placement="top">
+                        <IconButton component={Link} to="/login">
+                        <KeyboardReturn color="white" />
+                        </IconButton>
+                    </Tooltip>
+           </nav>
         )
     }
 }
-
 export default NavBar
