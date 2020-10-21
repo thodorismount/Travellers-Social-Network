20be580128a1e80a9850d63391b70d337582e892
FeSoner <it164826@it.teithe.gr>
2020-10-21 21:04:50 +0300

    Added the base of redux  and configured the registration form-state

--- client/src/components/GenderSelector.js
+++ client/src/components/GenderSelector.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -6,45 +6,43 @@ import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   formControl: {
     //margin: theme.spacing(1),
     minWidth: 120,
-    marginTop: "9px",
-    marginLeft: "30px"
-
+    marginTop: '9px',
+    marginLeft: '30px'
   },
   selectEmpty: {
-    marginTop: theme.spacing(2),
-  },
+    marginTop: theme.spacing(2)
+  }
 }));
 
-export default function SimpleSelect() {
-    const classes = useStyles();
-    const [gender, setGender] = React.useState('');
-  
-    const handleChange = (event) => {
-      setGender(event.target.value);
-    };
-  
-    return (
-        <div>
+export default function SimpleSelect(props) {
+  const classes = useStyles();
+  const [gender, setGender] = useState('');
 
-            <FormControl variant="outlined" className={classes.formControl}>
-                <InputLabel id="demo-simple-select-outlined-label">Gender</InputLabel>
-                    <Select
-                        labelId="demo-simple-select-outlined-label"
-                        id="demo-simple-select-outlined"
-                        value={gender}
-                        onChange={handleChange}
-                        label="Gender"
-                    >
-                        <MenuItem value={"male"}>Male</MenuItem>
-                        <MenuItem value={"female"}>Female</MenuItem>
-                        <MenuItem value={"other"}>Other</MenuItem>
-                    </Select>
-            </FormControl>  
+  const handleChange = event => {
+    setGender(event.target.value);
+    props.onChange(event.target.value);
+  };
 
-        </div>
-    );
+  return (
+    <div>
+      <FormControl variant='outlined' className={classes.formControl}>
+        <InputLabel id='demo-simple-select-outlined-label'>Gender</InputLabel>
+        <Select
+          labelId='demo-simple-select-outlined-label'
+          id='demo-simple-select-outlined'
+          value={gender}
+          onChange={handleChange}
+          label='Gender'
+        >
+          <MenuItem value={'male'}>Male</MenuItem>
+          <MenuItem value={'female'}>Female</MenuItem>
+          <MenuItem value={'other'}>Other</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
 }
--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,159 +1,159 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
-import PropTypes from 'prop-types'; //type-checking variables 
-import {Link} from 'react-router-dom';
+import PropTypes from 'prop-types'; //type-checking variables
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import CreateformDialog from '../components/FormDialog';
-import $ from "jquery";
-import NavBar from "../components/NavBar";
+import $ from 'jquery';
+import NavBar from '../components/NavBar';
 
-
-
-//MUI 
+//MUI
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import {blue} from '@material-ui/core/colors';
+import { blue } from '@material-ui/core/colors';
 import Card from '@material-ui/core/Card';
 import Divider from '@material-ui/core/Divider';
 
 const styles = {
-    form: {
-        textAlign: 'center'
-    },
-    pageTitle: {
-        margin: '10px auto auto auto'
-    },
-    textField: {
-        margin: '15px auto auto auto'
-    },
-    button: {
-        margin: '10px 10px'
-    },
-    logo: {
-        margin: '300px',
-        height: 'auto',
-        width: 'auto'
+  form: {
+    textAlign: 'center'
+  },
+  pageTitle: {
+    margin: '10px auto auto auto'
+  },
+  textField: {
+    margin: '15px auto auto auto'
+  },
+  button: {
+    margin: '10px 10px'
+  },
+  logo: {
+    margin: '300px',
+    height: 'auto',
+    width: 'auto'
+  },
+  card: {
+    padding: '10px',
+    marginTop: '180px',
+    backgroundColor: '#F0F2F5'
+  }
+};
 
-    },
-    card:{
-        padding: '10px',
-        marginTop: '180px',
-        backgroundColor: "#F0F2F5"
-    }
-}
-
- class login extends Component {
-     constructor(){
-         super();
-         this.state = {
-             email: '',
-             password: '',
-             loading: false,
-             error: {}
-         }
-     }
-     
-     componentDidMount(){
-        var contents = $('#navigationbar')[0];
-        contents.style.display="none";
-     }
-  
-     componentWillUnmount(){
-        var contents = $('#navigationbar')[0];
-        contents.style.display="flex";
-     }
-     
-     handleSubmit =(event) => {
-
-        event.preventDefault();
-        this.setState({
-            loading: true
-        });
-        const userData = {
-            email: this.state.email,
-            password: this.state.password
-        }
-        axios.post('/login', userData)
-            .then(res => {
-                console.log(res.data);
-                this.setState({
-                    loading: false
-                });
-                this.props.history.push('/'); //successful login directs user to the homepage
-
-            })
-            .catch(err => {
-                this.setState({
-                    error: err.response.data,
-                    loading: false
-                })
-            })
+class login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      loading: false,
+      error: {}
     };
-    handleChange = (event) => {
+  }
+
+  componentDidMount() {
+    var contents = $('#navigationbar')[0];
+    contents.style.display = 'none';
+  }
+
+  componentWillUnmount() {
+    var contents = $('#navigationbar')[0];
+    contents.style.display = 'flex';
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({
+      loading: true
+    });
+    const userData = {
+      email: this.state.email,
+      password: this.state.password
+    };
+    axios
+      .post('/login', userData)
+      .then(res => {
+        console.log(res.data);
         this.setState({
-            [event.target.name]: event.target.value
+          loading: false
         });
-    }
+        this.props.history.push('/'); //successful login directs user to the homepage
+      })
+      .catch(err => {
+        this.setState({
+          error: err.response.data,
+          loading: false
+        });
+      });
+  };
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
 
-    render() {
-        const {classes} = this.props;
-        const {errors, loading} = this.state;
-        return (
-            <Grid container className={classes.form}>
-                <Grid item sm>
-                    <h1 className={classes.logo}>Logo</h1>
-                </Grid>
-                <Grid item sm>
-                   <Card className={classes.card}>
-                        <Typography varient="h1" className={classes.pageTitle}>
-                            Please Login or Sign up
-                        </Typography>
-                        <form noValidate onSubmit={this.handleSubmit}>
-                            <TextField id="email"
-                                    name="email"
-                                    type="email"
-                                    label="Email"
-                                    variant="outlined"
-                                    className={classes.textField}
-                                // helperText={errors.email}
-                                // error={errors.email ? true: false}
-                                    value={this.state.email}
-                                    onChange={this.handleChange}
-                                    fullWidth/>
-                            <TextField id="password"
-                                    name="password"
-                                    type="password"
-                                    label="Password"
-                                    variant="outlined"
-                                    className={classes.textField}
-                                // helperText={errors.password}
-                                // error={errors.password ? true: false}
-                                    value={this.state.password}
-                                    onChange={this.handleChange}
-                                    fullWidth/>
-                            <Button type="submit"
-                                    size="large"
-                                    variant="contained"
-                                    color="primary"
-                                    className={classes.button}>
-                                Login
-                            </Button>
-                            <Divider variant="middle" className={classes.divider} />
-                            <CreateformDialog/>
-
-                        </form>
-                    </Card>
-                </Grid>
-                <Grid item sm/>
-            </Grid>
-
-        );
-    }
+  render() {
+    const { classes } = this.props;
+    const { errors, loading } = this.state;
+    return (
+      <Grid container className={classes.form}>
+        <Grid item sm>
+          <h1 className={classes.logo}>Logo</h1>
+        </Grid>
+        <Grid item sm>
+          <Card className={classes.card}>
+            <Typography varient='h1' className={classes.pageTitle}>
+              Please Login or Sign up
+            </Typography>
+            <form noValidate onSubmit={this.handleSubmit}>
+              <TextField
+                id='email'
+                name='email'
+                type='email'
+                label='Email'
+                variant='outlined'
+                className={classes.textField}
+                // helperText={errors.email}
+                // error={errors.email ? true: false}
+                value={this.state.email}
+                onChange={this.handleChange}
+                fullWidth
+              />
+              <TextField
+                id='password'
+                name='password'
+                type='password'
+                label='Password'
+                variant='outlined'
+                className={classes.textField}
+                // helperText={errors.password}
+                // error={errors.password ? true: false}
+                value={this.state.password}
+                onChange={this.handleChange}
+                fullWidth
+              />
+              <Button
+                type='submit'
+                size='large'
+                variant='contained'
+                color='primary'
+                className={classes.button}
+              >
+                Login
+              </Button>
+              <Divider variant='middle' className={classes.divider} />
+              <CreateformDialog />
+            </form>
+          </Card>
+        </Grid>
+        <Grid item sm />
+      </Grid>
+    );
+  }
 }
 
 login.propTypes = {
-    classes: PropTypes.object.isRequired
-}
+  classes: PropTypes.object.isRequired
+};
 export default withStyles(styles)(login);
