e6fd2ef447a65f3ec08b62216382e6cec90ed731
katerinap99 <62098281+katerinap99@users.noreply.github.com>
2020-10-25 23:32:49 +0200

    changed alignment

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import login from './pages/login';
 import signup from './pages/signup';
 import createUserProfile from './pages/createUserProfile';
 
+
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -27,11 +28,7 @@ class App extends Component {
                 <Route exact path='/home' component={home} />
                 <Route exact path='/login' component={login} />
                 <Route exact path='/signup' component={signup} />
-                <Route
-                  exact
-                  path='/createuserprofile'
-                  component={createUserProfile}
-                />
+                <Route exact path='/createuserprofile'   component={createUserProfile}  />
               </Switch>
             </div>
           </Router>
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -4,22 +4,32 @@ import Button from '@material-ui/core/Button';
 import Create from '@material-ui/icons/Create';
 import CreatePostDialog from '../components/CreatePostDialog';
 import PostCard from '../components/PostCard';
-
+var style = document.createElement('style');
+  style.innerHTML = `
+  #target {
+    display: flex;
+    justify-content:flex-end;
+  }
+  `;
+  document.head.appendChild(style);
 
 export class home extends Component {
     render() {
         return (
-            <Grid container >
-             <Grid item sm={6} xs={6}>
-                <PostCard />
-                <PostCard />
-                <PostCard />
+            <div>
+        <Grid id="target"  container spacing={3}>
+             <Grid  item xs={4}>
+                <CreatePostDialog />
             </Grid>
-             <Grid item sm={6} xs={6}>
-             <CreatePostDialog/>
-             </Grid>
-            </Grid>
-            
+         </Grid>
+        <Grid justify={'center'}  container spacing={3}>
+        <Grid item xs={4}>
+           <PostCard />
+           <PostCard />
+           <PostCard />
+       </Grid>
+    </Grid>
+    </div>
 
 
         )
