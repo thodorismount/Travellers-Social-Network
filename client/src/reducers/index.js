2f1f60f29c842bc693c151678c7c6a1acf86b692
StelKal <72509261+StelKal@users.noreply.github.com>
2020-11-02 12:11:20 +0200

    testing posts-redux (#32)
    rip

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,10 +3,13 @@ import alert from './alert';
 import loginAlert from './loginAlert';
 import auth from './auth';
 import profile from './profile';
+import post from './post';
+
 
 export default combineReducers({
   alert,
   loginAlert,
   auth,
-  profile
+  profile,
+  post
 });
