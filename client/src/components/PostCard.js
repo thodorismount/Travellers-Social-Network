998d296aca9e23af9bfc10b80c7e6a0625c782e3
FeSoner <it164826@it.teithe.gr>
2020-11-09 14:00:49 +0200

    Fixed a bug where interests did not load properly. Added links to post names and avatars that link to user profiles

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -62,18 +62,30 @@ function PostCard(props) {
     <Card className={classes.root}>
       <CardHeader
         avatar={
-          <Avatar
-            aria-label='recipe'
-            src='girl_female_woman_avatar-512.png'
-            className={classes.avatar}
-          ></Avatar>
+          <Link
+            to={`/userProfile/${props.user && props.user}`}
+            style={{ textDecoration: 'none', color: '#000' }}
+          >
+            <Avatar
+              aria-label='recipe'
+              src='/static/images/girl_female_woman_avatar-512.png'
+              className={classes.avatar}
+            ></Avatar>
+          </Link>
         }
         action={
           props.user === (props.authUser && props.authUser._id) ? (
             <ManagePost />
           ) : null
         }
-        title={props.username}
+        title={
+          <Link
+            to={`/userProfile/${props.user && props.user}`}
+            style={{ textDecoration: 'none', color: '#000' }}
+          >
+            {props.username}
+          </Link>
+        }
         subheader={`${props.location}, ${props.date}`}
         classes={{ title: classes.titleFont, subheader: classes.subheaderFont }}
         style={{ textTransform: 'capitalize' }}
