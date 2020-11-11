8a1ebd1ea9c0b7dcd4c57064732b07d60a1ff603
Thodoris Mountouris <32576758+thodorismount@users.noreply.github.com>
2020-11-11 18:07:50 +0200

    added travel experience progress bar on user profile which scales depending on the number of visited countries (#61)

---
+++ client/src/components/CountriesVisitedProgressBar.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import LinearProgress from '@material-ui/core/LinearProgress';
+
+const styles = makeStyles({
+  root: {
+    width: '100%'
+  }
+});
+
+export default function CountriesVisitedProgressBar(props) {
+  const classes = styles;
+
+  return (
+    <div className={classes.root}>
+      <LinearProgress
+        variant='determinate'
+        value={props.travelExperience * 10}
+        style={{
+          height: '0.5rem',
+          borderRadius: '10px'
+        }}
+      />
+    </div>
+  );
+}
--- client/src/components/Profile/Spinner.js
+++ client/src/components/Profile/Spinner.js
@@ -1,12 +1,25 @@
 import React, { Fragment } from 'react';
 import spinner from './spinner.gif';
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { makeStyles } from '@material-ui/core/styles';
 
-export default () => (
-  <Fragment>
-    <img
-      src={spinner}
-      alt='Loading...'
-      style={{ width: '200px', margin: 'auto', display: 'block' }}
-    />
-  </Fragment>
-);
+const useStyles = makeStyles(theme => ({
+  backdrop: {
+    backgroundColor: 'transparent',
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff'
+  }
+}));
+
+export default function Spinner() {
+  const classes = useStyles();
+
+  return (
+    <Fragment>
+      <Backdrop className={classes.backdrop} open={true}>
+        <CircularProgress fontSize='large' />
+      </Backdrop>
+    </Fragment>
+  );
+}
