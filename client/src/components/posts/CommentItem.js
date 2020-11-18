5860cb2a903c0dfa33fac6a9bbf663c7e55989ad
Thodoris Mountouris <32576758+thodorismount@users.noreply.github.com>
2020-11-18 22:36:05 +0200

    Change style in posts like comments (#92)
    * like counter disappears when likes are <0, used material ui List for displaying comments
    
    * changed comments styling, delete button, added box-shadow

--- client/src/components/posts/CommentItem.js
+++ client/src/components/posts/CommentItem.js
@@ -1,23 +1,71 @@
-
 import React from 'react';
 import Divider from '@material-ui/core/Divider';
-import {removeComment} from '../../actions/post';
+import { removeComment } from '../../actions/post';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteForeverOutlinedIcon from '@material-ui/icons/DeleteForeverOutlined';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
 
-
-const CommentItem = ({ postId, comment: {_id, user, text, name, date }, auth, removeComment }) => {
+const CommentItem = ({
+  postId,
+  comment: { _id, user, text, name, date },
+  auth,
+  removeComment
+}) => {
   return (
     <div>
       <div style={{ margin: '0.9rem' }}>
-        <h4 style={{ lineHeight: '0.9rem'}}>{name}</h4>
+        {/* <h4 style={{ lineHeight: '0.9rem' }}>{name}</h4>
         <p>{text}</p>
-        {user === (auth.user && auth.user._id)  && (
-        <button onClick={ e =>  removeComment(  postId, _id) } type="button" className="btn btn-danger">
-          
-        </button>
-      )}
-        <Divider/>
+        {user === (auth.user && auth.user._id) && ( */}
+        {/* // <IconButton onClick={e => removeComment(postId, _id)}> */}
+        {/* //   <DeleteForeverOutlinedIcon fontSize='medium' color='secondary' /> */}
+        {/* // </IconButton> */}
+        <ListItem>
+          <ListItemText
+            primary={
+              <React.Fragment>
+                <Typography variant='h6' color='textPrimary' component={'span'}>
+                  <Link
+                    href={`/userProfile/${auth.user && auth.user._id}`}
+                    color='textPrimary'
+                  >
+                    {name}
+                  </Link>
+                </Typography>
+              </React.Fragment>
+            }
+            secondary={
+              <React.Fragment>
+                <Typography
+                  variant='subtitle1'
+                  color='textPrimary'
+                  component={'span'}
+                >
+                  {text}
+                </Typography>
+              </React.Fragment>
+            }
+          />
+          {user === (auth.user && auth.user._id) && (
+            <ListItemSecondaryAction>
+              <IconButton
+                edge='end'
+                aria-label='delete'
+                onClick={e => removeComment(postId, _id)}
+              >
+                <DeleteForeverOutlinedIcon style={{ color: '#EE0101' }} />
+              </IconButton>
+            </ListItemSecondaryAction>
+          )}
+        </ListItem>
+        <Divider />
       </div>
     </div>
   );
@@ -25,12 +73,12 @@ const CommentItem = ({ postId, comment: {_id, user, text, name, date }, auth, re
 
 CommentItem.propTypes = {
   auth: PropTypes.object.isRequired,
-  comment:PropTypes.object.isRequired,
+  comment: PropTypes.object.isRequired,
   removeComment: PropTypes.func.isRequired
-}
+};
 
 const mapStateToProps = state => ({
   auth: state.auth
-})
+});
 
-export default connect(mapStateToProps, {removeComment}) (CommentItem);
+export default connect(mapStateToProps, { removeComment })(CommentItem);
